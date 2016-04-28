142102fd82f1e18e984f5170bf4f2b5374f12706
Sean Anderson <sean.martin.anderson@gmail.com>
2016-04-28 16:14:25 -0500

    memory

--- tutorials/final-si/control.sh
+++ tutorials/final-si/control.sh
@@ -2,14 +2,14 @@
 export OMP_NUM_THREADS=12
 export OMP_STACKSIZE=1G
 
-# RPA (~3 GB, 40.69 s @ 12 core) RPA doesn't work with openMP?
+# RPA (~170 MB, 40.69 s @ 12 core) RPA doesn't work with openMP?
 cd rpa/
 mpiexec.hydra -np 12 ~/bin-2013/dp-5.2.99-r1883/bin/dp-5.3.99-mpi -i rpa.in -k si.kss 2>&1 | tee rpa.out
 printf "0\n0\n0\n0.030\n" | ~/bin-2013/dp-5.2.99-r1883/bin/broad outlf.mdf 0.030gr_chi.mdf
 rm log_* mem_* out.kdotp_* tree_*
 cd ..
 
-# GWRPA (~2 GB, 40.69 s @ 12 core)
+# GWRPA (~170 MB, 40.69 s @ 12 core)
 cd gwrpa/
 mpiexec.hydra -np 12 ~/bin-2013/dp-5.2.99-r1883/bin/dp-5.3.99-mpi -i gwrpa.in -k si.kss 2>&1 | tee gwrpa.out
 printf "0\n0\n0\n0.030\n" | ~/bin-2013/dp-5.2.99-r1883/bin/broad outlf.mdf 0.030gr_chi.mdf
