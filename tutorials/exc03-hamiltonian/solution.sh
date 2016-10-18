77a267a6865f27aff038f32b510c3d2431d01c48
Sean Anderson <sean.martin.anderson@gmail.com>
2016-10-18 14:31:24 +0200

    nice

--- tutorials/exc03-hamiltonian/solution.sh
+++ tutorials/exc03-hamiltonian/solution.sh
@@ -7,9 +7,13 @@ EXC_OMP="/opt/science/bin/dp-5.2.99-r1883-intel13.1.117/bin/dp-5.3.99-openmp"
 EXC_MPI="/opt/science/bin/dp-5.2.99-r1883-intel13.1.117/bin/dp-5.3.99-mpi"
 BROAD="/opt/science/bin/dp-5.2.99-r1883-intel13.1.117/bin/broad"
 
-# Necessary OMP variables
+# OpenMP variables
 export OMP_STACKSIZE=1G
 export OMP_NUM_THREADS=64
+
+# MPI variables
+MPI_PROCS=192
+MPI_HOSTS="fat1,fat2,fat3"
 
 abinit_mpi () {
     local CASE=${1}
@@ -39,49 +43,37 @@ exc_openmp () {
 exc_mpi () {
     local CASE=${1}
     mkdir -p ${CASE}
-    mpiexec.hydra -np 192 -hosts fat1,fat2,fat3 \
+    mpiexec.hydra -np ${MPI_PROCS} -hosts ${MPI_HOSTS} \
     $EXC_MPI -i ${CASE}.in -k si.kss -s si.scr 2>&1 | tee ${CASE}.out
     if [ -e "outexc.mdf" ]; then broad outexc.mdf 0.05; fi
     rm tree_* log_* mem_* out.kdotp_*
-    mv mem tree ${CASE}.out out*.mdf ${CASE}/
+    mv mem tree out*.mdf ${CASE}.out ${CASE}/
 }
 
 
-# Creating the KSS and SCR files with ABINIT (~1 min)
-abinit_mpi si_gen
+abinit_mpi abinit # Create the KSS and SCR files with ABINIT (~1 min, 12 cores)
+ln -sf abinit/si_o_DS2_KSS si.kss # Link the KSS and SCR files generated 
+ln -sf abinit/si_o_DS3_SCR si.scr # with ABINIT in the previous step
 
-# Link the KSS and SCR files generated with ABINIT in the previous step
-ln -sf si_gen/si_o_DS2_KSS si.kss
-ln -sf si_gen/si_o_DS3_SCR si.scr
+exc_openmp exc01-TD_FD # Tamm-Dancoff, serial diagonalization (~5 min, 64 cores)
 
-# Tamm-Dancoff, full diagonalization, serial (~5 min)
-exc_openmp exc01-TD_FD
+exc_openmp exc02-TD_HY # Tamm-Dancoff, parallel Haydock (~0.5 min, 64 cores)
 
-# Tamm-Dancoff, Haydock diagonalization, parallel (~0.5 min)
-exc_openmp exc02-TD_HY
+# exc_openmp exc03-FH_HY # BAD! Coupling, parallel Haydock (~1 min, 64 cores)
 
-# DOES NOT WORK! Coupling, Haydock diagonalization, parallel (~1 min)
-#exc_openmp exc03-FH_HY
+exc_openmp exc04a-FH_FD # Coupling, creation of hamiltonian (~2 min, 64 cores)
+                        # Step 1: creation of the hamiltonian and write to disk
 
-# Coupling, full diagonalization, parallel (creation of the hamiltonian, ~2 min)
-exc_openmp exc04a-FH_FD
+mv exc04a-FH_FD/out.exh out.exh     # Dumb move command, read below
+mv exc04a-FH_FD/out.exc out.exc     # Dumb move command, read below
+mv exc04a-FH_FD/out.kdotp out.kdotp # Dumb move command, read below
+ln -sf out.exh in.exh       # Linking output from previous step for 
+ln -sf out.exc in.exc       # MPI diagonalization. Files must be in the same
+ln -sf out.kdotp in.kdotp   # directory as links
 
-# Linking output from previous step for MPI diagonalization. Must be in same
-# directory, hence the dumb move commands
-mv exc04a-FH_FD/out.exh out.exh
-mv exc04a-FH_FD/out.exc out.exc
-mv exc04a-FH_FD/out.kdotp out.kdotp
-ln -sf out.exh in.exh
-ln -sf out.exc in.exc
-ln -sf out.kdotp in.kdotp
+exc_mpi exc04b-FH_FD # Coupling, diagonalization of hamiltonian 
+                     # Step 2: full MPI diagonalization (~1 hour, 192 cores)
 
-# Coupling, MPI full diagonalization (inverting the hamiltonian, ~1 hour)
-exc_mpi exc04b-FH_FD 
-
-# Moce shit around and clean, all finished!
-unlink si.kss
-unlink si.scr
-unlink in.exc
-unlink in.exh
-unlink in.kdotp
-mv out.* exc04a-FH_FD
+mv out.* exc04a-FH_FD   # Move hamiltonian back
+rm si.{kss,scr}         # Get rid of links
+rm in.{exc,exh,kdotp}   # Get rid of links
